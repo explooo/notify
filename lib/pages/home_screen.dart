e486d6c315fdfb6b206d6259612b428acb81b9bd
Divyam Thakur <thakurd398@gmail.com>
2024-02-12 16:11:56 +0530

    added ability to press and hold notes

--- lib/pages/home_screen.dart
+++ lib/pages/home_screen.dart
@@ -37,24 +37,60 @@ class SearchBarDelegate extends SearchDelegate<String> {
 }
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key}) : super(key: key);
-
+  HomeScreen({Key? key}) : super(key: key);
+  final selectedNotes = ValueNotifier<List<Map<String, dynamic>>>([]);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Notify'),
-        actions: [
-          IconButton(
-            icon: Icon(Icons.search),
-            onPressed: () {
-              showSearch(
-                context: context,
-                delegate: SearchBarDelegate(),
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(kToolbarHeight),
+        child: ValueListenableBuilder<List<Map<String, dynamic>>>(
+          valueListenable: selectedNotes,
+          builder: (context, value, child) {
+            if (value.isEmpty) {
+              // No notes are selected, show the regular AppBar
+              return AppBar(
+                title: Text('Notify'),
+                actions: [
+                  IconButton(
+                    icon: Icon(Icons.search),
+                    onPressed: () {
+                      showSearch(
+                        context: context,
+                        delegate: SearchBarDelegate(),
+                      );
+                    },
+                  ),
+                ],
               );
-            },
-          ),
-        ],
+            } else {
+              // Some notes are selected, show the custom AppBar
+              return AppBar(
+                leading: IconButton(
+                  icon: Icon(Icons.arrow_back),
+                  onPressed: () {
+                    selectedNotes.value = [];
+                  },
+                ),
+                title: Text('${value.length} selected'),
+                actions: [
+                  IconButton(
+                    icon: Icon(Icons.archive),
+                    onPressed: () {
+                      // Archive the selected notes...
+                    },
+                  ),
+                  IconButton(
+                    icon: Icon(Icons.delete),
+                    onPressed: () {
+                      // Delete the selected notes...
+                    },
+                  ),
+                ],
+              );
+            }
+          },
+        ),
       ),
       body: StreamBuilder<List<Map<String, dynamic>>>(
         stream: DatabaseHelper.instance.notesStream,
@@ -65,27 +101,52 @@ class HomeScreen extends StatelessWidget {
           } else if (snapshot.hasError) {
             return Center(child: Text('Error: ${snapshot.error}'));
           } else {
+            return ValueListenableBuilder<List<Map<String, dynamic>>>(
+        valueListenable: selectedNotes,
+        builder: (context, value, child) {
             return ListView.builder(
               itemCount: snapshot.data!.length,
               itemBuilder: (context, index) {
                 Map<String, dynamic> note = snapshot.data![index];
-                return ListTile(
-                  title: Text(note['title']),
-                  subtitle: Text(note['content']),
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => NoteEditingScreen(note: note),
-                      ),
-                    );
+                return GestureDetector(
+                  onLongPress: () {
+                    selectedNotes.value = List.from(selectedNotes.value)
+                      ..add(note);
                   },
-                );
-              },
-            );
-          }
+                  onTap: () {
+                    if (selectedNotes.value.isNotEmpty) {
+                      // Some notes are already selected, add this note to the selected notes
+                      if (!selectedNotes.value.any((selectedNote) =>
+                          selectedNote['_id'] == note['_id'])) {
+                        selectedNotes.value = List.from(selectedNotes.value)
+                          ..add(note);
+                      }
+                    } else {
+                      // No notes are selected, navigate to the NoteEditingScreen
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => NoteEditingScreen(note: note),
+                        ),
+                      );
+                    }
+                  },
+                  child: ListTile(
+                    tileColor: selectedNotes.value.any(
+                            (selectedNote) => selectedNote['_id'] == note['_id'])
+                        ? Colors.grey
+                        : null,
+                    title: Text(note['title']),
+                    subtitle: Text(note['content']),
+                    ),
+              );
+            },
+          );
         },
-      ),
+      );
+    }
+  },
+),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           Navigator.push(
